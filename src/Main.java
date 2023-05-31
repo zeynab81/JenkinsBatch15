d526ee77137f32764974572fe2e7b069adbe50ce
zeynab81 <mamakhovazeynab@gmail.com>
2023-05-30 20:19:06 -0400

    zeynab81

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("Hello world!");
+            Thread.sleep(2000);
+        }
+    }
+}
