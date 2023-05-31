46e2821f233c362578fdc137aa5f0a5ce0fe3397
zeynab81 <mamakhovazeynab@gmail.com>
2023-05-31 15:05:09 -0400

    Minor change

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i <10 ; i++) {
+        for (int i = 0; i <20 ; i++) {
             System.out.println("Hello world!");
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         }
     }
 }
